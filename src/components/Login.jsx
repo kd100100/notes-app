20f841aa757dff43b765c5cee5fad2636ed750ca
Keerthivasan D <keerthivasan.d@thoughtworks.com>
2022-05-08 23:23:39 +0530

    Update login button

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,7 +4,9 @@ import "../assets/Loader.css";
 import Logo from "../assets/logo.png";
 
 const Login = ({ setUser }) => {
-	useEffect(() => {
+	const [loggingIn, setLoggingIn] = React.useState(false);
+
+	const loginUsingGoogle = () => {
 		const provider = new GoogleAuthProvider();
 		const auth = getAuth();
 		signInWithPopup(auth, provider)
@@ -15,12 +17,26 @@ const Login = ({ setUser }) => {
 			.catch((error) => {
 				console.log(error);
 			});
-	}, []);
+	};
 	return (
 		<div className="w-screen h-screen bg-white">
 			<div className="flex flex-col items-center justify-center h-screen">
 				<img src={Logo} alt="Logo" className="mb-10 w-96" />
-				<div class="lds-hourglass"></div>
+				<div className="h-40">
+					{loggingIn ? (
+						<div class="lds-hourglass"></div>
+					) : (
+						<button
+							onClick={() => {
+								setLoggingIn(true);
+								loginUsingGoogle();
+							}}
+							className="bg-violet-500 text-white py-2 px-4 rounded-lg text-xl"
+						>
+							Login with Google
+						</button>
+					)}
+				</div>
 			</div>
 		</div>
 	);
